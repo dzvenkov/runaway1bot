232fdc53a94576e305b8718ddce07fc7d1a04f60
dzvenkov <dzvenkov@gmail.com>
2024-02-02 21:43:35 +0000

    repack inference into class

--- inference.py
+++ inference.py
@@ -1,17 +1,25 @@
 from realesrgan import RealESRGANer
 from basicsr.archs.rrdbnet_arch import RRDBNet
+from PIL import Image
 import cv2
 
-model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
+class RealESRGAN_Inference():
+    def __init__(self) -> None:
+        self.model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
+        self.upsampler = RealESRGANer(
+                scale=2,
+                model_path="RealESRGAN_x2plus.pth",
+                model=self.model,
+                half=False)
 
-upsampler = RealESRGANer(
-        scale=2,
-        model_path="RealESRGAN_x2plus.pth",
-        model=model,
-        half=False)
+    def upsample(self, image : Image):
+        output, _ = self.upsampler.enhance(image, outscale=2)
+        return output
+    
 
-path = "sample.jpeg"
-img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-output, _ = upsampler.enhance(img, outscale=2)
-
-cv2.imwrite("output.jpeg", output)
+if __name__ == '__main__':
+        path = "sample.jpeg"
+        img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
+        inf = RealESRGAN_Inference()
+        output = inf.upsample(img)
+        cv2.imwrite("output.jpeg", output)
