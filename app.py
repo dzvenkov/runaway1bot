b0327437ef6757045e18a50a7693b8d8365eeb4a
dzvenkov <dzvenkov@gmail.com>
2024-02-02 23:12:25 +0000

    implemented local version of upscale inference

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ import asyncio
 from PIL import Image
 import os
 import logging
-
+from inference import RealESRGAN_Inference
+import cv2
+import time
 
 #prep
 logging.basicConfig(level=logging.DEBUG)
@@ -31,23 +33,22 @@ async def on_startup(_):
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     logging.info("processing /start")
-    await message.answer("[local app]Welcome! This bot upscales any image you send it using Real-ESGRAN model.")
-
-@dp.message(Command("test"))
-async def cmd_test(message: types.Message):
-    logging.info("begin processing /test")
-    await message.answer("[local app]start test")
-    await asyncio.sleep(3)
-    await message.answer(f"finish test! Hello World! incoming message date {message.date}")
-    logging.info("end processing /test")
+    await message.answer("[local app]Welcome! This bot upscales any image you send it using <a href='https://github.com/xinntao/Real-ESRGAN'>RealESGRAN_x2plus model</a>.", parse_mode='HTML')
 
 @dp.message(Command("help"))
 async def cmd_help(message: types.Message):
-    await message.answer("Upload any image and it will be upscaled using Real-ESGRAN model.")
+    await asyncio.sleep(1)
+    await message.answer("Upload any image and it will be upscaled using <a href='https://github.com/xinntao/Real-ESRGAN'>RealESGRAN_x2plus model</a>.", parse_mode='HTML')
 
 @dp.message(F.photo)
 async def download_photo(message: types.Message, bot: Bot):
-    #pass it through filesystem, not optimal but simplifies debugging
+    rotate = message.caption and "/rotate" in message.caption
+
+    await asyncio.sleep(0.5)
+
+    if not rotate:
+        await message.answer("Nice picture! Ok, now I'll upscale it x2, this may take couple of minutes, please be patient");
+
     os.makedirs("./downloads", exist_ok=True)
     os.makedirs("./results", exist_ok=True)
     source_filename = f"./downloads/{message.photo[-1].file_id}.jpg"
@@ -56,17 +57,32 @@ async def download_photo(message: types.Message, bot: Bot):
         message.photo[-1],
         destination=source_filename
     )
-    with Image.open(source_filename) as img:
-        img.rotate(180).save(result_filename)
+    
+    start_time = time.time()
+    if rotate:
+        with Image.open(source_filename) as img:
+            img.rotate(180).save(result_filename)
+        result_message = f"Here's the rotated image, it took {time.time() - start_time:.1f} seconds to make it"
+    else:
+        image = cv2.imread(source_filename, cv2.IMREAD_UNCHANGED)
+        inference = RealESRGAN_Inference()
+        output = inference.upsample(image)
+        cv2.imwrite(result_filename, output)
+        result_message = f"Here's the upscaled image, it took {time.time() - start_time:.1f} seconds to make it"
 
     processed_image = types.FSInputFile(result_filename)
-    result = await message.answer_photo(
+    await message.answer_photo(
         processed_image,
-        caption="Rotated image"
+        caption=result_message
     )
 
-    
 #webhook handler
+async def process(request):
+    dict = await request.json()
+    logging.debug(f"Decoded as {type(dict)}: {dict}")
+    update_result = await dp.feed_raw_update(bot, dict)
+    logging.debug(f"Update result: {update_result} {type(update_result)}")    
+
 async def handle_webhook(request):
     url = str(request.url)
     index = url.rfind('/')
@@ -74,10 +90,7 @@ async def handle_webhook(request):
     
     if token == tg_bot_token:
         logging.debug(f"Received request {request}")
-        dict = await request.json()
-        logging.debug(f"Decoded as {type(dict)}: {dict}")
-        update_result = await dp.feed_raw_update(bot, dict)
-        logging.debug(f"Update result: {update_result} {type(update_result)}")
+        asyncio.create_task(process(request))
         resp = web.Response();
         logging.debug(f"prepared response: {resp}/body={resp.body}")
         return resp
